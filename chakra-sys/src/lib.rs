6f83790458a336a862200f0d47a447cfffd9874e
bIgBV <bhargav.voleti93@gmail.com>
2020-09-28 22:09:31 -0700

    Add all the bindings for types from io_uring.h

--- chakra-sys/src/lib.rs
+++ chakra-sys/src/lib.rs
@@ -63,7 +63,7 @@ pub union buf_index_padding {
 }
 
 #[repr(C)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct personality {
     pub buf_or_group: libc::__u16,
     pub personality: libc::__u16,
@@ -157,6 +157,7 @@ pub const SPLICE_F_FD_IN_FIXED: libc::__u32 = 1 << 31;
 
 /// A Completion Queue Event.
 #[repr(C)]
+#[derive(Debug)]
 pub struct io_uring_cqe {
     user_data: libc::__u64,
     res: libc::__s32,
@@ -172,6 +173,140 @@ pub const IORING_CQE_BUFFER_SHIFT: libc::__u32 = 16;
 pub const IORING_OFF_SQ_RING: libc::__u64 = 0;
 pub const IORING_OFF_CQ_RING: libc::__u64 = 0x8000000;
 pub const IORING_OFF_SQES: libc::__u64 = 0x10000000;
+
+// Filled with the offset for mmap(2)
+#[repr(C)]
+#[derive(Debug)]
+pub struct io_sqring_offsets {
+    pub head: libc::__u32,
+    pub tail: libc::__u32,
+    pub ring_mask: libc::__u32,
+    pub ring_entries: libc::__u32,
+    pub flags: libc::__u32,
+    pub dropped: libc::__u32,
+    pub array: libc::__u32,
+    pub resv1: libc::__u32,
+    pub resv2: libc::__u64,
+}
+
+// sq_ring->flags
+// Needs io_uring_enter to wakeup
+pub const IORING_SQ_NEED_WAKEUP: libc::c_uint = 1 << 0;
+// CQ ring is overflown
+pub const IORING_SQ_CQ_OVERFLOW: libc::c_uint = 1 << 1;
+
+// cq_ring->flags
+#[repr(C)]
+#[derive(Debug)]
+pub struct io_cqring_offsets {
+    pub head: libc::__u32,
+    pub tail: libc::__u32,
+    pub ring_mask: libc::__u32,
+    pub ring_entries: libc::__u32,
+    pub overflow: libc::__u32,
+    pub cqes: libc::__u32,
+    pub flags: libc::__u32,
+    pub resv1: libc::__u32,
+    pub resv2: libc::__u64,
+}
+
+// cq_ring->flags
+// disable eventfd notifications
+pub const IORING_CQ_EVENTFD_DISABLED: libc::c_uint = 1 << 0;
+
+// io_uring_enter flags
+pub const IORING_ENTER_GETEVENTS: libc::c_uint = 1 << 0;
+pub const IORING_ENTER_SQ_WAKEUP: libc::c_uint = 1 << 1;
+pub const IORING_ENTER_SQ_WAIT: libc::c_uint = 1 << 2;
+
+/// Parameters passed in for io_uring_setup(2). Copied back with updated info
+/// on success
+#[repr(C)]
+#[derive(Debug)]
+pub struct io_uring_params {
+    pub sq_entries: libc::__u32,
+    pub cq_entries: libc::__u32,
+    pub flags: libc::__u32,
+    pub sq_thread_cpu: libc::__u32,
+    pub sq_thread_idle: libc::__u32,
+    pub features: libc::__u32,
+    pub wq_fd: libc::__u32,
+    pub resv: [libc::__u32; 3],
+    pub sq_off: io_sqring_offsets,
+    pub cq_off: io_cqring_offsets
+}
+
+// io_uring_params->features flags
+pub const IORING_FEAT_SINGLE_MMAP: libc::c_uint = 1 << 0;
+pub const IORING_FEAT_NODROP: libc::c_uint = 1 << 1;
+pub const IORING_FEAT_SUBMIT_STABLE: libc::c_uint = 1 << 2;
+pub const IORING_FEAT_RW_CUR_POS: libc::c_uint = 1 << 3;
+pub const IORING_FEAT_CUR_PERSONALITY: libc::c_uint = 1 << 4;
+pub const IORING_FEAT_FAST_POLL: libc::c_uint = 1 << 5;
+pub const IORING_FEAT_POLL_32BITS: libc::c_uint = 1 << 6;
+pub const IORING_FEAT_SQPOLL_NONFIXED: libc::c_uint = 1 << 7;
+
+// io_uring_register(2) opcodes and arguments
+pub const IORING_REGISTER_BUFFERS: libc::c_uint = 0;
+pub const IORING_UNREGISTER_BUFFERS: libc::c_uint = 1;
+pub const IORING_REGISTER_FILES: libc::c_uint = 2;
+pub const IORING_UNREGISTER_FILES: libc::c_uint = 3;
+pub const IORING_REGISTER_EVENTFD: libc::c_uint = 4;
+pub const IORING_UNREGISTER_EVENTFD: libc::c_uint = 5;
+pub const IORING_REGISTER_FILES_UPDATE: libc::c_uint = 6;
+pub const IORING_REGISTER_EVENTFD_ASYNC: libc::c_uint = 7;
+pub const IORING_REGISTER_PROBE: libc::c_uint = 8;
+pub const IORING_REGISTER_PERSONALITY: libc::c_uint = 9;
+pub const IORING_UNREGISTER_PERSONALITY: libc::c_uint = 10;
+pub const IORING_REGISTER_RESTRICTIONS: libc::c_uint = 11;
+pub const IORING_REGISTER_ENABLE_RINGS: libc::c_uint = 12;
+
+pub const IO_URING_OP_SUPPORTED: libc::c_uint = 1 << 0;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct io_uring_probe_op {
+    pub op: libc::__u8,
+    pub resv: libc::__u8,
+    pub flags: libc::__u16,
+    pub resv2: libc::__u32,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct io_uring_probe {
+    pub last_op: libc::__u8,
+    pub ops_len: libc::__u8,
+    pub resv: libc::__u16,
+    pub resv2: [libc::__u32; 3],
+    pub ops: [io_uring_probe_op; 0],
+}
+
+#[repr(C)]
+pub struct io_uring_restriction {
+    pub opcode: libc::__u16,
+    pub restriction_op: restriction_op,
+    pub resv: libc::__u8,
+    pub resv2: [libc::__u32; 3],
+}
+
+#[repr(C)]
+pub union restriction_op {
+    pub register_op: libc::__u8,
+    pub sqe_op: libc::__u8,
+    pub sqe_flags: libc::__u8,
+}
+
+// io_uring_restriction->opcode values
+// allow an io_uring_register(2) opcode
+pub const IORING_RESTRICTION_REGISTER_OP: libc::c_uint = 0;
+// Allow an sqe opcode
+pub const IORING_RESTRICTION_SQE_OP: libc::c_uint = 1;
+// Allow sqe flags
+pub const IORING_RESTRICTION_SQE_FLAGS_ALLOWED: libc::c_uint = 2;
+// Require sqe flags (these flags must be set on each submission)
+pub const IORING_RESTRICTION_SQE_FLAGS_REQUIRED: libc::c_uint = 3;
+
 
 #[link(name = "uring")]
 extern "C" {
